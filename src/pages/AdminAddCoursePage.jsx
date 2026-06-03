c05338f5d4b593e1cffe6d01a2554bae6488e663
Miaad Asjad <miaadasjad1514@gmail.com>
2026-06-03 18:42:46 +0500

    update add course

--- src/pages/AdminAddCoursePage.jsx
+++ src/pages/AdminAddCoursePage.jsx
@@ -1,7 +1,7 @@
 
 
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import Loader from "../components/Loader";
